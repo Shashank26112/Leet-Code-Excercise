1f6fe733afd2556ddd9cf3e78c932e9885487d23
Shashank Katti <90169675+Shashank26112@users.noreply.github.com>
2024-01-20 09:46:49 +0530

    Create JavaFX GUI .java

---
+++ 0030-substring-with-concatenation-of-all-words/JavaFX GUI .java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class JavaFXExample extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Example");
+
+        Button btn = new Button("Click me!");
+        btn.setOnAction(e -> System.out.println("Button clicked!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+
+        primaryStage.show();
+    }
+}
